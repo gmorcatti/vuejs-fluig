79b65beef99f5354343c15b4d60c00c2e7632ec5
gmorcatti <gabriel.figueiredo@upflow.me>
2020-05-11 19:03:48 -0300

    Organização do código em objetos. (Não é possível separar template do html index)

--- main.js
+++ main.js
@@ -1,74 +1,104 @@
-$( document ).ready(function() {
-    
-    Vue.directive('money', {
-        inserted(el, binding){
-            const amount = parseFloat(el.innerHTML).toFixed(2)
-                .replace('.',',')
-                .replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
-            el.innerHTML = `${binding.value} ${amount}`
-        }
-    })
-    
-    Vue.component('modal', {
-        props: ['tipo'],
-        data: function(){
-            return{
-                nome: 0
+var vue = {
+    init(){
+        //Inicia o Vue, deixe sempre o init ao final do código.
+        
+        // eventBus é para emitir eventos.
+        var eventBus = new Vue();
+        // Vue.prototype.$eventHub = new Vue();
+        
+        new Vue({
+            el: "#app",
+            data: {
+                nome: ''
             }
-        },
-        template: $('#modal').html(),
-        methods: {
-            submit(){
-                $('#myModal').on('shown.bs.modal', function () {
-                    $('#myInput').trigger('focus')
-                })
+        })
+
+    },
+    components: function(){
+        /* Componente: Modal
+        A ideia é este componente ser composto pelo botão e pelo html interno do modal.
+        ----------------- ADICIONAR ----------------
+        prop: name - Todos elementos dentro do modal serem compostos por esse name + diferencial.
+        */
+        Vue.component('modal', {
+            props: ['tipo'],
+            data: function(){
+                return{
+                    nome: 0
+                }
             },
-            money(){
-                if(this.tipo == "money"){
-                    $('[name="jorge"]').maskMoney({
-                        prefix: '',
-                        thousands: '.',
-                        decimal: ','
-                    });
+            template: $('#modal').html(),
+            methods: {
+                submit(){
+                    $('#myModal').on('shown.bs.modal', function () {
+                        $('#myInput').trigger('focus')
+                    })
+                },
+                money(){
+                    if(this.tipo == "money"){
+                        $('[name="jorge"]').maskMoney({
+                            prefix: '',
+                            thousands: '.',
+                            decimal: ','
+                        });
+                    }
                 }
             }
-        }
-    })
+        })
 
-    Vue.component('input-label', {
-        props: ['tipo', 'label', 'name'],
-        data: function(){
-            return {
-                
+        /*  Componente: Input-Label
+            propriedade: [
+                tipo: "money" ou "quantidade" (para colocar o mask money corretamente).
+                label: O texto da label.
+                name: atributo name do input e for do label.
+            ]
+        */
+        Vue.component('input-label', {
+            props: ['tipo', 'label', 'name'],
+            data: function(){
+                return {
+                    
+                }
+            },
+            template: $("#input-label").html(),
+            mounted: function(){
+                console.log("[name='"+this.name+"']")
+                // if(this.tipo == "money"){
+                //     $("[name='"+this.name+"']").maskMoney({
+                //         prefix: 'R$',
+                //         thousands: '.',
+                //         decimal: ','
+                //     });
+                // } else if(this.tipo == "quantidade"){
+                //     $("[name='"+this.name+"']").maskMoney({
+                //         prefix: '',
+                //         thousands: '.',
+                //         decimal: ','
+                //     });
+                // }
             }
-        },
-        template: $("#input-label").html(),
-        mounted: function(){
-            console.log("[name='"+this.name+"']")
-            if(this.tipo == "money"){
-                $("[name='"+this.name+"']").maskMoney({
-                    prefix: 'R$',
-                    thousands: '.',
-                    decimal: ','
-                });
-            } else if(this.tipo == "quantidade"){
-                $("[name='"+this.name+"']").maskMoney({
-                    prefix: '',
-                    thousands: '.',
-                    decimal: ','
-                });
+            
+        })
+    },
+    directives: function(){
+        // A diretiva do tipo v-money coloca o R$ no inicio e a virgula com os centavos ao final.
+        // Não funciona como mask money, mas como parseReais.
+        Vue.directive('money', {
+            componentUpdated(el, binding){
+                const amount = parseFloat(el.innerHTML).toFixed(2)
+                    .replace('.',',')
+                    .replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
+                el.innerHTML = `${binding.value} ${amount}`
             }
-        }
-        
-    })
+        })
+    }
+
+}
+
+$( document ).ready(function() {
     
-    var eventBus = new Vue();
-    // Vue.prototype.$eventHub = new Vue();
+    vue.directives()
+    vue.components()
+    vue.init()
     
-    new Vue({
-        el: "#app",
-        data: {
-            nome: ''
-        }
-    })
 });
