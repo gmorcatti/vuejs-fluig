9f15a6c127ff990050cbecde32aa8d2c7b06c48b
gmorcatti <gabriel.figueiredo@upflow.me>
2020-05-13 09:00:39 -0300

    Correção de alguns bugs no modal component e adicionado txt com planejamento

--- main.js
+++ main.js
@@ -44,6 +44,7 @@ var vue = {
         // }
     })
 
+    Vue.component("select2", {
         /* Componente: Select2
             Implementação do select2
             propriedade: [
@@ -52,82 +53,90 @@ var vue = {
                 database: Se refere a qual a base de dados será utilizada para exibir as opções
             ]
         */
-        Vue.component("select2", {
-            template: $('#select2').html(),
-            props: {
-                name: {
-                    type: String,
-                },
-                label: {},
-                database: {
-                    default: 0
-                }
+        template: $('#select2').html(),
+        props: {
+            name: {
+                type: String,
             },
-            data(){
-                return {
-                    dados: {
-                        0: [],
-                        1: [
-                        "Ave",
-                        "Macaco",
-                        "Girafa",
-                        "MORANDO"
-                        ],
-                        2: [
-                        "NEM",
-                        "FER",
-                        "RANDO"
-                    ]},
-                }
-            },
-            beforeMount() {
-                var cases = this.database
-
-                // Caso seja acrescentado algum dado que deve vir através do retorno de uma consulta, acrescentar o array no switch abaixo.
-                switch(cases){
-                    case '3':
-                    case '4':
-                        this.dados[cases] = ["MANGALO", "GARGALO"]
-                        break
-                                 
-                }
-
-            },
-            mounted: function(){
-                var component = this
-                $('.' + component.name + ' .uf-select2').select2({
-                    placeholder: 'Selecione uma opção',
-                    language: {
-                        noResults: function () {
-                                return "Nenhum resultado encontrado.";
-                        }
-                    }
-                });
-
-                $('.'+ component.name + ' .selection').on('click', function(){
-                        
-                    $(component.dados[component.database]).each(function(index, element){
-                        
-                        //Este if procura algum componente no select que tenha um value igual ao inserido, caso haja ele não é inserido novamente.
-                        if ( $('.' + component.name + ' .uf-select2').find(`option[value="${element}"]`).length == 0 ) {
-                            
-                            //Cria a a opção no select (seu valor é igual ao campo 'id' e o texto igual ao campo 'text')
-                            var mudançaOpção = new Option(element, element, false, false);
-                            
-                            // Da append nessa opção
-                            $('.' + component.name + ' .uf-select2').append(mudançaOpção).trigger('change');
-        
-                        }
-        
-                    })
-                    
-                    // Após carregar os novos elementos o select deve ser fechado e reaberto para renderizar estes elementos.
-                    $('.' + component.name + ' .uf-select2').select2('close').select2('open')
-        
-                })
-                
+            label: {},
+            database: {
+                default: 0
             }
-        })
+        },
+        data(){
+            return {
+                dados: {
+                    0: [],
+                    1: [
+                    "Ave",
+                    "Macaco",
+                    "Girafa",
+                    "MORANDO"
+                    ],
+                    2: [
+                    "NEM",
+                    "FER",
+                    "RANDO"
+                ]},
+            }
+        },
+        beforeMount() {
+            var cases = this.database
+
+            // Caso seja acrescentado algum dado que deve vir através do retorno de uma consulta, acrescentar o array no switch abaixo.
+            switch(cases){
+                case '3':
+                case '4':
+                    this.dados[cases] = ["MANGALO", "GARGALO"]
+                    break
+                                
+            }
+
+        },
+        mounted: function(){
+            var component = this
+            var uf_select2 = $('.' + component.name + ' .uf-select2')
+
+            uf_select2.select2({
+                placeholder: 'Selecione uma opção',
+                language: {
+                    noResults: function () {
+                            return "Nenhum resultado encontrado.";
+                    }
+                }
+            });
+
+            $('.'+ component.name + ' .select2-selection').on('click',function(){
+                
+                var baseDados = component.dados[component.database]
+                
+                $(baseDados).each(function(index, element){
+                    
+                    //Este if procura algum componente no select que tenha um value igual ao inserido, caso haja ele não é inserido novamente.
+                    if ( uf_select2.find(`option[value="${element}"]`).length == 0 ) {
+                        
+                        //Cria a a opção no select (seu valor é igual ao campo 'id' e o texto igual ao campo 'text')
+                        var addOption = new Option(element, element, false, false);
+                        
+                        // Da append nessa opção
+                        uf_select2.append(addOption);
+    
+                    }
+    
+                })
+
+                uf_select2.trigger('change');
+                // Após carregar os novos elementos o select deve ser fechado e reaberto para renderizar estes elementos.
+                uf_select2.select2('close').select2('open')
+                
+            })
+            
+            uf_select2.on('focusin',function(){
+                $('.'+ component.name + ' .select2-selection').trigger('click')
+            })
+            
+        }
+    })
 
         /* Componente: Modal
             A ideia é este componente ser composto pelo botão e pelo html interno do modal.
@@ -153,9 +162,16 @@ var vue = {
                 
             },
             mounted: function(){
-                if(this.cleanclose){
-                    $('#myModal .close, .modal-footer .close-footer').on('click', function(){
-                        $('#myModal input').each(function(i, e){
+                var component = this
+                if(component.cleanclose){
+
+                    console.log(this.name)
+                    $('[modal="'+component.name+'"] .close, [modal="'+component.name+'"] .modal-dialog .modal-footer .close-footer').on('click', function(){
+                        console.log("aqui")
+                        console.log('[modal="'+component.name+'"]')
+
+                        $('[modal="'+component.name+'"] input').each(function(i, e){
+                            console.log("Aqui n")
                             var $el = $(e)
                             $el.val("")
                         })
@@ -173,13 +189,17 @@ var vue = {
         */
         Vue.component('input-label', {
             template: $("#input-label").html(),
-            props: ['tipo', 'label', 'name'],
-            data: function(){
-                return {
-                    
-                }
+            props: {
+                tipo: {},
+                label: {},
+                name: {},
+                readonly: { type: Boolean },
             },
             mounted: function(){
+
+                if(this.readonly){
+                    $('[name="' + this.name + '"]').attr("readonly", "true")
+                }
 
                 if(this.tipo == "money"){
                     $("[name='"+this.name+"']").maskMoney({
