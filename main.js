f3f8430e9d0a64ff1c635c8b36889e19cb0893e4
gmorcatti <gabriel.figueiredo@upflow.me>
2020-06-14 10:16:45 -0300

    Alteração da versão antiga para a nova versão do projeto
    A nova versão já tem o componente de datas e select2. Além de ter algumas correções de bugs das versões anteriores.

--- main.js
+++ main.js
@@ -14,34 +14,200 @@ var vue = {
         })
 
     },
+
     components: function(){
+        Vue.component("datas", {
+            /* Componente: datas
+                Implementação do datas
+                propriedade: [
+                    name: Nome do elemento base
+                    label: Texto do label do componente
+                    database: Se refere a qual a base de dados será utilizada para exibir as opções
+                ]
+            */
+        template: $('#data').html(),
+        props: {
+            name: {
+                type: String,
+            },
+            label: {},
+            start_date: {},
+            end_date: {}
+        },
+        data(){
+            return {
+                dateSettings: {
+                    startDate: this.start_date,
+                    endDate: this.end_date
+                }
+            }
+        },
+        mounted: function(){ 
+            var component = this
+            console.log(this.start_date)
+            if(component.start_date == "today"){
+                console.log("OVO")
+                component.dateSettings.startDate = moment().format("DD/MM/YYYY")
+            }
+            if(component.end_date == "today"){
+                component.dateSettings.endDate = moment().format("DD/MM/YYYY")
+            }
+
+
+            $('#'+ component.name).datepicker({
+                format: 'dd/mm/yyyy',
+                language: 'pt-BR',
+                autoclose: true,
+                startDate: component.dateSettings.startDate,
+                endDate: component.dateSettings.endDate
+            });
+            
+        }
+    })
+
+    Vue.component("select2", {
+        /* Componente: Select2
+            Implementação do select2
+            propriedade: [
+                name: Nome do elemento base
+                label: Texto do label do componente
+                database: Se refere a qual a base de dados será utilizada para exibir as opções
+                multiple: Se é possível selecionar mais de uma opção
+                search: Se é possível realizar a busca da opção desejada (OBS: Multiple a busca é feita pelo próprio campo)
+            ]
+        */
+        template: $('#select2').html(),
+        props: {
+            name: {
+                type: String,
+            },
+            label: {},
+            database: {
+                default: 0
+            },
+            multiple: { type: Boolean },
+            search: { type: Boolean }
+        },
+                                        
+        data(){
+            return {
+                searchSize: 0,
+                dados: {
+                    0: [],
+                    1: [
+                    "Ave",
+                    "Macaco",
+                    "Girafa",
+                    "MORANDO"
+                    ],
+                    2: [
+                    "1 indiozinho",
+                    "2 indiozinhos",
+                    "10 o bote"
+                ]},
+            }
+        },
+        beforeMount() {
+            var cases = this.database
+
+            // Caso seja acrescentado algum dado que deve vir através do retorno de uma consulta, acrescentar o array no switch abaixo.
+            switch(cases){
+                case '3':
+                case '4':
+                    this.dados[cases] = ["MANGALO", "GARGALO"]
+                    break
+                                
+            }
+
+            if(this.search){
+                this.searchSize = Infinity
+            }
+
+        },
+        mounted: function(){
+            var component = this
+            var uf_select2 = $('.' + component.name + ' .uf-select2')
+
+            uf_select2.select2({
+                placeholder: 'Selecione uma opção',
+                language: {
+                    noResults: function () {
+                            return "Nenhum resultado encontrado.";
+                    }
+                },
+                minimumResultsForSearch: component.searchSize
+            });
+
+            $('.'+ component.name + ' .select2-selection').on('click',function(){
+                
+                var baseDados = component.dados[component.database]
+                
+                $(baseDados).each(function(index, element){
+                    
+                    //Este if procura algum componente no select que tenha um value igual ao inserido, caso haja ele não é inserido novamente.
+                    if ( uf_select2.find(`option[value="${element}"]`).length == 0 ) {
+                        
+                        //Cria a a opção no select (seu valor é igual ao campo 'id' e o texto igual ao campo 'text')
+                        var addOption = new Option(element, element, false, false);
+                        
+                        // Da append nessa opção
+                        uf_select2.append(addOption);
+    
+                    }
+    
+                })
+
+                uf_select2.trigger('change');
+                // Após carregar os novos elementos o select deve ser fechado e reaberto para renderizar estes elementos.
+                uf_select2.select2('close').select2('open')
+                
+            })
+            
+            uf_select2.on('focusin',function(){
+                $('.'+ component.name + ' .select2-selection').trigger('click')
+            })
+            
+        }
+    })
+
         /* Componente: Modal
-        A ideia é este componente ser composto pelo botão e pelo html interno do modal.
-        ----------------- ADICIONAR ----------------
-        prop: name - Todos elementos dentro do modal serem compostos por esse name + diferencial.
+            A ideia é este componente ser composto pelo botão e pelo html interno do modal.
+            propriedade: [
+                name: Define o nome do modal, além disso todos elementos ativos devem receber o name + "Complemento"
+                cleanclose: Tipo Boolean (Se tem o attr é true, se não tem é false)
+                        O modal ao ser fechado salva suas informações ou é sempre limpo.
+                
+            ]
         */
         Vue.component('modal', {
-            props: ['tipo'],
+            template: $('#modal').html(),
+            props: {
+                name: {},
+                cleanclose: { type: Boolean },
+            },
             data: function(){
                 return{
                     nome: 0
                 }
             },
-            template: $('#modal').html(),
             methods: {
-                submit(){
-                    $('#myModal').on('shown.bs.modal', function () {
-                        $('#myInput').trigger('focus')
+                
+            },
+            mounted: function(){
+                var component = this
+                if(component.cleanclose){
+
+                    console.log(this.name)
+                    $('[modal="'+component.name+'"] .close, [modal="'+component.name+'"] .modal-dialog .modal-footer .close-footer').on('click', function(){
+                        console.log("aqui")
+                        console.log('[modal="'+component.name+'"]')
+
+                        $('[modal="'+component.name+'"] input').each(function(i, e){
+                            console.log("Aqui n")
+                            var $el = $(e)
+                            $el.val("")
+                        })
                     })
-                },
-                money(){
-                    if(this.tipo == "money"){
-                        $('[name="jorge"]').maskMoney({
-                            prefix: '',
-                            thousands: '.',
-                            decimal: ','
-                        });
-                    }
                 }
             }
         })
@@ -54,28 +220,32 @@ var vue = {
             ]
         */
         Vue.component('input-label', {
-            props: ['tipo', 'label', 'name'],
-            data: function(){
-                return {
-                    
-                }
-            },
             template: $("#input-label").html(),
+            props: {
+                tipo: {},
+                label: {},
+                name: {},
+                readonly: { type: Boolean },
+            },
             mounted: function(){
-                console.log("[name='"+this.name+"']")
-                // if(this.tipo == "money"){
-                //     $("[name='"+this.name+"']").maskMoney({
-                //         prefix: 'R$',
-                //         thousands: '.',
-                //         decimal: ','
-                //     });
-                // } else if(this.tipo == "quantidade"){
-                //     $("[name='"+this.name+"']").maskMoney({
-                //         prefix: '',
-                //         thousands: '.',
-                //         decimal: ','
-                //     });
-                // }
+
+                if(this.readonly){
+                    $('[name="' + this.name + '"]').attr("readonly", "true")
+                }
+
+                if(this.tipo == "money"){
+                    $("[name='"+this.name+"']").maskMoney({
+                        prefix: 'R$',
+                        thousands: '.',
+                        decimal: ','
+                    });
+                } else if(this.tipo == "quantidade"){
+                    $("[name='"+this.name+"']").maskMoney({
+                        prefix: '',
+                        thousands: '.',
+                        decimal: ','
+                    });
+                }
             }
             
         })
@@ -84,7 +254,8 @@ var vue = {
         // A diretiva do tipo v-money coloca o R$ no inicio e a virgula com os centavos ao final.
         // Não funciona como mask money, mas como parseReais.
         Vue.directive('money', {
-            componentUpdated(el, binding){
+            inserted(el, binding){
+                
                 const amount = parseFloat(el.innerHTML).toFixed(2)
                     .replace('.',',')
                     .replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
@@ -100,5 +271,5 @@ $( document ).ready(function() {
     vue.directives()
     vue.components()
     vue.init()
-    
+
 });
